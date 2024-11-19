a22f4d71893b3ec68f6c1c510a1b5f8eb5b06a53
georgifly23 <georgikanchev23@gmail.com>
2024-11-19 02:28:17 +0200

    Refactor Sidebar layout; increase width and adjust fixed button positioning

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,23 @@
 import { ToggleMode } from "../ToggleMode/ToggleMode";
 import styles from "./Sidebar.module.css";
+
 const Sidebar = () => {
- 
-return (
+  return (
     <div className={styles.sidebar}>
-        <div className="join join-vertical" style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-            <button className="btn join-item">DM's</button>
-            <div style={{ marginTop: '50px' }}>
-                <button className="btn join-item">Channel 1</button>
-                <button className="btn join-item">Channel 2</button>
-                <button className="btn join-item">Channel 3</button>
-            </div>
-            <div style={{ marginTop: '370px' }}>
-                <button className="btn join-item">+</button>
-                <ToggleMode />
-            </div>
+      <div className="join join-vertical" style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <button className="btn join-item">DM's</button>
+        <div style={{ marginTop: '50px' }}>
+          <button className="btn join-item">Channel 1</button>
+          <button className="btn join-item">Channel 2</button>
+          <button className="btn join-item">Channel 3</button>
         </div>
+        <div className={styles.fixedBottom}>
+          <button className="btn join-item">+</button>
+          <ToggleMode />
+        </div>
+      </div>
     </div>
-);
+  );
 };
 
 export default Sidebar;
