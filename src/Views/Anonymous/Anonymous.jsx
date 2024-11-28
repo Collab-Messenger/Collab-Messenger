0142c46e43f68eed42b427ab07b7382c4469cde9
Kristian Palovski <Kristian_Palovski@abv.bg>
2024-11-28 13:13:44 +0200

    user and team counter

--- src/Views/Anonymous/Anonymous.jsx
+++ src/Views/Anonymous/Anonymous.jsx
@@ -1,10 +1,52 @@
+import { useState, useEffect } from 'react';
+import { db } from '../../config/firebase-config';
+import { ref, get, child } from 'firebase/database';
+import styles from './Anonymous.module.css';
 
 export function Anonymous() {
+    const [userCount, setUserCount] = useState(0);
+    const [teamCount, setTeamCount] = useState(0);
 
+    useEffect(() => {
+        const fetchUserCount = async () => {
+            try {
+                const dbRef = ref(db);
+                const snapshot = await get(child(dbRef, 'users'));
+                if (snapshot.exists()) {
+                    const users = snapshot.val();
+                    setUserCount(Object.keys(users).length);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        const fetchTeamCount = async () => {
+            try {
+                const dbRef = ref(db);
+                const snapshot = await get(child(dbRef, 'teams'));
+                if (snapshot.exists()) {
+                    const teams = snapshot.val();
+                    setTeamCount(Object.keys(teams).length);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUserCount();
+        fetchTeamCount();
+    }, []);
 
     return (
-        <div>
-            <h1>Anonymous</h1>
+        <div className={styles.container}>
+            <div className={styles.caption}>Welcome to the LinkUp App!</div>
+            <div className={styles['platform-statistics']}>
+                <h2>Platform Statistics</h2>
+                <div className={styles.stats}>
+                    <p>Users: {userCount}</p>
+                    <p>Teams: {teamCount}</p>
+                </div>
+            </div>
         </div>
     );
 }
