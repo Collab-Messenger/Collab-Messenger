c1cde46b2aa7615ae4f375983b6c114a40f5b67c
georgifly23 <georgikanchev23@gmail.com>
2024-12-11 04:44:59 +0200

    Not Found message update

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,8 +4,25 @@
  */
 export const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="ml-[400px] self-center">
+      <h1 className="text-2xl font-bold mb-4">404: Oops, Lost in the Chat Void!</h1>
+      <div className="text-lg">
+        <h1>
+It seems you've wandered into the Internet Bermuda Triangle! 🌐</h1>
+
+<h1>
+If you're looking for a video call: Try dialing again—maybe the internet ghosts picked it up. 👻</h1>
+<h1>
+If you're searching for a chat: The messages might be stuck in emoji purgatory. 😜🎉</h1>
+<h1>
+Until we find what you're looking for, why not try hitting the back button or starting a new conversation?</h1>
+<h1>
+Who knows, maybe the missing page is just shy. 😅
+</h1>
+<h1>
+
+🎬 PS: No videos or chats were harmed in the making of this error.</h1>
+</div>
     </div>
   );
 };
